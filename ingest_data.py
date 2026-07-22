73469689f36e8c8362327bcb334d2a51a99dcef8
Eric Clavel <e23clavel@gmail.com>
2026-07-22 14:05:44 -0700

    added ingest data container to docker-compose

--- ingest_data.py
+++ ingest_data.py
@@ -6,6 +6,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 
 
@@ -33,22 +34,23 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-
-    chunksize = 100000
-
+@click.command()
+@click.option('--pg_user', default='root', help='PostgreSQL user')
+@click.option('--pg_password', default='root', help='PostgreSQL password')
+@click.option('--pg_host', default='localhost', help='PostgreSQL host')
+@click.option('--pg_port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg_db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of taxi data')
+@click.option('--month', default=1, type=int, help='Month of taxi data')
+@click.option('--target_table', default='yellow_taxi_data', help='Target table name in database')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+@click.option('--url', default=None, help='URL to CSV file (if not provided, will be constructed from year/month)')
+def run(pg_user, pg_password, pg_host, pg_port, pg_db, year, month, target_table, chunksize, url):
+    """Ingest taxi data into PostgreSQL database."""
+    
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    if url is None:
+        url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
     engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
 
